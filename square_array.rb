571e100642d44348e6e893d66c8507e2f0ff7553
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-06 05:07:27 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,14 @@
-def square_array(array)
-  new_array = []
-  array.each do |num|
-    sq = num ** 2
-    new_array.push(sq)
-  end
-  new_array
-end
+# def square_array(array)
+#   new_array = []
+#   array.each do |num|
+#     sq = num ** 2
+#     new_array.push(sq)
+#   end
+#   new_array
+# end
 
+def square_array(array)
+  sq_array = []
+  array.each { |x| sq_array.push(x**2)
+  return sq_array
+end
